790cb90bfdca2f4cc94ec60ad7319b7e6a964d7d
shige0309 <82074248+shige0309@users.noreply.github.com>
2022-07-20 18:24:37 +0900

    deploy

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+const taskRouter = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+app.use(express.json());
+app.use(express.static('./public'));
+
+//ルーティング設計
+app.use('/api/v1/tasks', taskRouter);
+
+//データベースと接続
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, console.log('サーバーが起動しました。'));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
